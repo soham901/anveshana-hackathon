be5ed37c1e1a125d5d356a07045668b9c156ae00
Soham <sagathiyasoham12345@gmail.com>
2024-09-10 06:27:37 +0530

    add: reactnativeelements

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,59 +1,47 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, ScrollView, View } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+import { Button, Card, Icon, SearchBar, Text } from "@rneui/themed";
+import { Dialog } from "@rneui/themed";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [visible, setVisible] = useState(false);
+  const toggleDialog = () => setVisible(!visible);
+
+  const [search, setSearch] = useState("");
+  const onSearch = (value: string) => setSearch(value);
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      }
+    >
+      <SearchBar value={search} onChangeText={onSearch} />
+      <Button title={"Search"} onPress={toggleDialog} />
+      <Dialog isVisible={visible} onBackdropPress={toggleDialog}>
+        <Dialog.Title title="Dialog Title" />
+        <Text>Dialog body text. Add relevant information here.</Text>
+      </Dialog>
+
+      <Crops />
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -65,6 +53,126 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
+  },
+  container: {
+    flex: 1,
+  },
+  fonts: {
+    marginBottom: 8,
+  },
+  user: {
+    flexDirection: "row",
+    marginBottom: 6,
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
+  name: {
+    fontSize: 16,
+    marginTop: 5,
   },
 });
+
+const Crops = () => {
+  const users = [
+    {
+      name: "brynn",
+      avatar: "https://uifaces.co/our-content/donated/1H_7AxP0.jpg",
+    },
+    {
+      name: "thot leader",
+      avatar:
+        "https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb",
+    },
+    {
+      name: "jsa",
+      avatar: "https://uifaces.co/our-content/donated/bUkmHPKs.jpg",
+    },
+    {
+      name: "talhaconcepts",
+      avatar: "https://randomuser.me/api/portraits/men/4.jpg",
+    },
+    {
+      name: "andy vitale",
+      avatar: "https://uifaces.co/our-content/donated/NY9hnAbp.jpg",
+    },
+    {
+      name: "katy friedson",
+      avatar:
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgxMTc1MTYzM15BMl5BanBnXkFtZTgwNzI5NjMwOTE@._V1_UY256_CR16,0,172,256_AL_.jpg",
+    },
+  ];
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <Card>
+          <Card.Title>CARD WITH DIVIDER</Card.Title>
+          <Card.Divider />
+          {users.map((u, i) => {
+            return (
+              <View key={i} style={styles.user}>
+                <Image
+                  style={styles.image}
+                  resizeMode="cover"
+                  source={{ uri: u.avatar }}
+                />
+                <Text style={styles.name}>{u.name}</Text>
+              </View>
+            );
+          })}
+        </Card>
+        <Card containerStyle={{ marginTop: 15 }}>
+          <Card.Title>FONTS</Card.Title>
+          <Card.Divider />
+          <Text style={styles.fonts} h1>
+            h1 Heading
+          </Text>
+          <Text style={styles.fonts} h2>
+            h2 Heading
+          </Text>
+          <Text style={styles.fonts} h3>
+            h3 Heading
+          </Text>
+          <Text style={styles.fonts} h4>
+            h4 Heading
+          </Text>
+          <Text style={styles.fonts}>Normal Text</Text>
+        </Card>
+        <Card>
+          <Card.Title>HELLO WORLD</Card.Title>
+          <Card.Divider />
+          <Card.Image
+            style={{ padding: 0 }}
+            source={{
+              uri: "https://awildgeographer.files.wordpress.com/2015/02/john_muir_glacier.jpg",
+            }}
+          />
+          <Text style={{ marginBottom: 10 }}>
+            The idea with React Native Elements is more about component
+            structure than actual design.
+          </Text>
+          <Button
+            icon={
+              <Icon
+                name="code"
+                color="#ffffff"
+                iconStyle={{ marginRight: 10 }}
+              />
+            }
+            buttonStyle={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0,
+            }}
+            title="VIEW NOW"
+          />
+        </Card>
+      </View>
+    </ScrollView>
+  );
+};
